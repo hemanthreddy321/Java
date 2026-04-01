7deb17284728b54fdcfc1c93fb9461b8146a938b
hemanthreddy321 <hemanthreddy12773@gmail.com>
2026-04-01 11:48:20 +0530

    Modified file

--- src/practice/Program.java
+++ src/practice/Program.java
@@ -2,11 +2,11 @@ package practice;
 
 public class Program {
     public static void main(String[] args) {
-        String name = "lol";
+        String name = "hemanth";
         String reverse = "";
 
-        for ( int i=0; i<name.length(); i++) {
-            reverse = name.charAt(i)+reverse;
+        for (int i = name.length() - 1; i >= 0; i--) {
+            reverse = reverse + name.charAt(i);
         }
         System.out.println(reverse);
         if (name.equalsIgnoreCase(reverse)) {
