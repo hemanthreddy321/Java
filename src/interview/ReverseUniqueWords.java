0645733dd3c6c9ac4e1e6a537370194053920126
hemanthreddy321 <hemanthreddy12773@gmail.com>
2026-04-21 12:05:26 +0530

    ReverseUniqueWords

---
+++ src/interview/ReverseUniqueWords.java
@@ -0,0 +1,22 @@
+package interview;
+
+import java.util.*;
+
+public class ReverseUniqueWords {
+    public static void main(String[] args) {
+        String input = "Good afternoon Good Evening";
+        String[] words = input.split(" ");
+        Set<String> s = new HashSet<>();
+        for (String a : words) {
+            s.add(a);
+        }
+        System.out.println(s);
+
+        String[] v = s.toArray(new String[0]);
+        String result = "";
+        for (int i = v.length - 1; i >= 0; i--) {
+            result = result + v[i] + " ";
+        }
+        System.out.println(result);
+    }
+}
