5a81990f8d7d2e11cdcdbd0e2b6e19bcdce803cf
hemanthreddy321 <hemanthreddy12773@gmail.com>
2026-04-25 21:34:52 +0530

    Modified

--- src/interview/CommonArrayElements.java
+++ src/interview/CommonArrayElements.java
@@ -23,3 +23,16 @@ public class CommonArrayElements {
                 }
         }
         }*/
+
+/*int[] arr1 = {1, 2, 3, 4};
+int[] arr2 = {3, 4, 5, 6};
+
+HashSet<Integer> set = new HashSet<>();
+for (int num : arr1) set.add(num);
+
+for (int num : arr2) {
+        if (set.contains(num)) {
+        System.out.println("Common element: " + num);
+    }
+            }*/
+
