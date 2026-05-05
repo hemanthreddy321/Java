772bddaa872316d1c264949d6629c9c50ed5ac87
hemanthreddy321 <hemanthreddy12773@gmail.com>
2026-05-05 16:43:33 +0530

    Added comments

--- src/strings/CountOccurencesOfCharacter.java
+++ src/strings/CountOccurencesOfCharacter.java
@@ -1,16 +1,40 @@
 package strings;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Iterator;
 
 public class CountOccurencesOfCharacter {
     public static void main(String[] args) {
         String str = "hemanth";
-        HashMap<Character, Integer> map = new HashMap<>();
+
+        // 1. Initialize a HashMap to store Character as Key and its Count as Value
+        HashMap<Character, Integer> map = new LinkedHashMap<>();
+
+        // 2. Convert string to char array and loop through each character
+        // .toCharArray() breaks "hemanth" into ['h','e','m','a','n','t','h']
+
         for (char ch : str.toCharArray()) {
+            // getOrDefault checks if the char exists; if yes, gets the value, else returns 0.
+            // Then we add 1 to that value and put it back in the map.
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        System.out.println(map);
 
+        // 3. To use an Iterator, we must access the "EntrySet" of the map
+        // This creates an iterator that can move through Key-Value pairs
+        Iterator<Map.Entry<Character, Integer>> iterator = map.entrySet().iterator();
+
+        System.out.println("Character Occurrences:");
+
+        // 4. Use a while loop to check if there is a next entry in the map
+        while (iterator.hasNext()) {
+            // Get the next Map Entry (the Key-Value pair)
+            Map.Entry<Character, Integer> entry = iterator.next();
+
+            // Print the Key (Character) and the Value (Count)
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
 
