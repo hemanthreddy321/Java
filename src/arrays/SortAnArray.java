fe5b993b5f6dda12e09649c7cf8ab8a0253dc6e7
hemanthreddy321 <hemanthreddy12773@gmail.com>
2026-05-07 12:51:41 +0530

    Updated

--- src/arrays/SortAnArray.java
+++ src/arrays/SortAnArray.java
@@ -6,6 +6,8 @@ public class SortAnArray {
     public static void main(String[] args) {
         String[] cars = {"Volvo", "BMW", "Tesla", "Ford", "Fiat", "Mazda", "Audi"};
         Arrays.sort(cars);
+        System.out.println(Arrays.toString(cars));
+
         for (String i : cars) {
             System.out.println(i);
         }
