b44c592f794f054f1ac3eb09f39a2741c78b362a
hemanthreddy321 <hemanthreddy12773@gmail.com>
2026-06-18 14:27:55 +0530

    Modified

--- src/strings/StringSplit.java
+++ src/strings/StringSplit.java
@@ -1,6 +1,6 @@
-package practice;
+package strings;
 
-public class Practice {
+public class StringSplit {
     static void main() {
         String text = "Order ID: 12345 successfully created";
         String orderId = text.split(":")[1].trim().split(" ")[0];
