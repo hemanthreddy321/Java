860801ff2173986d66474695a6ec91e5add4105e
hemanthreddy321 <hemanthreddy12773@gmail.com>
2026-07-14 10:24:26 +0530

    Modified

---
+++ src/strings/UniqueCharacters.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class UniqueCharacters {
+
+    public static void main(String[] args) {
+
+        String str = "selenium";
+
+        for(int i=0;i<str.length();i++){
+
+            char ch = str.charAt(i);
+
+            if(str.indexOf(ch)==str.lastIndexOf(ch)){
+                System.out.print(ch +" ");
+            }
+        }
+    }
+}
